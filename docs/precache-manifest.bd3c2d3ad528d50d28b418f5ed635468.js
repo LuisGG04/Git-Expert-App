d774a93cac3eab64f9d22634127a01a1f681178b
Luis Enrique Gaitan Gaytan <luis_unsc117@outlook.es>
2021-11-09 15:16:35 -0600

    firts commit

---
+++ docs/precache-manifest.bd3c2d3ad528d50d28b418f5ed635468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d4a582ec308bc8f578c92049d91b15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b27c16e33ad49c40b61",
+    "url": "/static/css/main.b84a8a9d.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b27c16e33ad49c40b61",
+    "url": "/static/js/main.f78557c5.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
